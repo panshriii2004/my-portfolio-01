774f00da707ec4fa387a05bff53131b0a2544435
Panshri <byeetoall@gmail.com>
2026-02-28 16:36:37 +0530

    Fix GitHub Pages basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
+  basePath: '/my-portfolio-01',
+  assetPrefix: '/my-portfolio-01/',
   images: {
     unoptimized: true,
   },
